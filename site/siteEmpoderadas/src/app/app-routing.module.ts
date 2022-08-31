aedf5cda8ec2c306abdefd1aff0ad1c988a36fb2
Luiz Henrique Santos <96305078+LuizHenriqueSantos15@users.noreply.github.com>
2022-08-31 12:53:38 -0300

    Criando Rotas

--- site/siteEmpoderadas/src/app/app-routing.module.ts
+++ site/siteEmpoderadas/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistasComponent } from './artistas/artistas.component';
+import { HomeComponent } from './home/home.component';
+import { PecasComponent } from './pecas/pecas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pecas', component: PecasComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  {path: 'artistas', component: ArtistasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- site/siteEmpoderadas/src/app/app.module.ts
+++ site/siteEmpoderadas/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PecasComponent } from './pecas/pecas.component';
+import { HomeComponent } from './home/home.component';
+import { ArtistasComponent } from './artistas/artistas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      PecasComponent,
+      HomeComponent,
+      ArtistasComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
